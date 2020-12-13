ee2d5394b2f3ce209fddbc94ee514098aa87e4f7
Batuhan <69036666+xretra@users.noreply.github.com>
2020-12-13 17:27:34 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const firstName = 'Noxus';
+const lastName = 'Weass';
+const fullName = 'Noxus Weass';
+const age = 100;
+const city = 'Dubai'
+var hobbies = 'Kitap Dergi Futbol Tenis';
+var url = 'https://www.google.com/eN coK araNan keLimeler'
+var mark = ["Apple","Huawei","Samsung","Nokia"];
+let val;
+
+
+// isim soyisim birleştirme işlemlerini yaparak bunları bir cümle haline getir.
+
+// isim ve soyisimin kaç karakter olduğnu yazdır
+
+// substring ile sadece firstName yazdır 
+
+// slice ile sadece lastName yazdır
+
+// fullName değişkenini 'Lendra Weass' olarak değiştirin
+
+// tüm cümleyi küçük ve büyük harfe çevirin(fullName içinden)
+
+// url, '.com' ifadesini içeriyor mu?
+
+// url string ifadesini geçerli bir url olarak düzenleyin
+
+// ekrana herhangi bir harfi yazdır (fullName içinden)
+
+// hobileri boşluktan sonra ayırarak yazdır
+
+// mark dizisi kaç elemanlıdır ?
+
+// "Casper" değerini dizinin başına ekleyin
+
+// "Apple" değerini silin
+
+// [5,1,87,80] dizisini sıralayın
+
+// "Nokia" değeri dizinin bir elemanı mıdır ?
+
+// mark dizisindeki son elemanı silin
+
+// mark dizisindeki ilk dizi ile son dizinin yerini değiştirin
+
+// mark dizisinin elemanlarını alfabetik olarak sıralayın.
+
